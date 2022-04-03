ecab2ec540c8dc0f30081d483d984c401f7f8425
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-03 15:32:20 -0500

    first go for loop

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -3,8 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	// Basic math stuff
 	x := 5
 	y := 7
 	sum := x + y
-	fmt.Print(sum)
+	fmt.Println(sum)
+
+	// Basic for loop
+	a := 1
+	if a > 6 {
+		fmt.Println("more than six")
+	} else if a > 2 {
+		fmt.Println("More than two but less than six")
+	} else {
+		fmt.Println("less than two")
+	}
+
 }
